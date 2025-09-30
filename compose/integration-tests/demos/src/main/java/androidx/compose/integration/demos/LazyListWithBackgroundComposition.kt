657fa13da0c587449161c6e595139d38f9819819
Aditya Sharat <adityasharat@meta.com>
2025-09-30 22:49:36 +0100

    more
    Change-Id: I35bc9d4ddac6f20d0e1dc908edc895b1eabcb799

--- compose/integration-tests/demos/src/main/java/androidx/compose/integration/demos/LazyListWithBackgroundComposition.kt
+++ compose/integration-tests/demos/src/main/java/androidx/compose/integration/demos/LazyListWithBackgroundComposition.kt
@@ -158,7 +158,7 @@ fun background(): Color {
 @Composable
 private fun Header(header: Item.Header) {
     Text(
-        text = "${header.text}",
+        text = header.text,
         fontSize = 20.sp,
         modifier = Modifier.background(color = background())
     )
