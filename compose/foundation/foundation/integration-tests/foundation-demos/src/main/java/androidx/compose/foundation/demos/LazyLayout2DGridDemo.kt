f314dd83409aa678dfc1614b54d6b8049d91b712
Aditya Sharat <adityasharat@meta.com>
2025-09-30 14:54:26 +0100

    more
    Change-Id: I53a62c35fb5516adaf1e042efb506711e57f6ac7

--- compose/foundation/foundation/integration-tests/foundation-demos/src/main/java/androidx/compose/foundation/demos/LazyLayout2DGridDemo.kt
+++ compose/foundation/foundation/integration-tests/foundation-demos/src/main/java/androidx/compose/foundation/demos/LazyLayout2DGridDemo.kt
@@ -144,7 +144,8 @@ fun Lazy2DGrid(
         itemProvider,
         modifier.draggable2D(state).then(state.remeasurementModifier),
         null,
-        gitmeasurePolicy,
+        precomposeState = null,
+        measurePolicy,
     )
 }
 
